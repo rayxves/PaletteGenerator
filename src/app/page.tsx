4d27677ff66207bf0cb456c65dc4e519fceb1a4a
Rayssa Guimarães <guimaraesr305@gmail.com>
2025-02-21 11:23:02 -0300

    add components at home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,50 @@
+"use client";
+
+import { useState } from "react";
+import ImageUploader from "./components/image-uploader";
+import ColorPalette from "./components/color-palette";
+
 export default function Home() {
-  return <div className="w-full h-full"></div>;
+  const [isImageUploaded, setIsImageUpload] = useState<boolean>(false);
+  const [progressFromImagePreview, setProgressFromImagePreview] =
+    useState<boolean>(false);
+  const [imageData, setImageData] = useState<ImageData>({} as ImageData);
+
+  const handleImageUpload = async (file: File) => {
+    const imageBitmap = await createImageBitmap(file, {
+      resizeWidth: 200,
+      resizeHeight: 200,
+    });
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+
+    if (!ctx) return;
+
+    canvas.width = imageBitmap.width;
+    canvas.height = imageBitmap.height;
+    ctx?.drawImage(imageBitmap, 0, 0);
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    if (imageData) {
+      setImageData(imageData);
+      setIsImageUpload(true);
+    } else {
+      setImageData({} as ImageData);
+      setIsImageUpload(false);
+    }
+    return imageData;
+  };
+
+  const handleImagePreviewProgress = () => {
+    setProgressFromImagePreview(true);
+  };
+
+  return (
+    <div className="w-full h-full">
+      {isImageUploaded && progressFromImagePreview ? (
+        <ColorPalette imageData={imageData} numColors={6} />
+      ) : (
+        <ImageUploader onImageUpload={handleImageUpload} onImagePreviewProgress={handleImagePreviewProgress}/>
+      )}
+    </div>
+  );
 }
